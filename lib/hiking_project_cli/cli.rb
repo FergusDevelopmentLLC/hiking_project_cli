87ae4d03e1c99eb790faccb4bd64c87f27526f6f
Will Carter <will.carter@fergusllc.com>
2020-01-31 20:36:49 -0700

    user interface mocked up

--- lib/hiking_project_cli/cli.rb
+++ lib/hiking_project_cli/cli.rb
@@ -1,5 +1,66 @@
 class HikingProjectCli::CLI
+
     def call
-        puts "Hello World"
+        #welcome message to the user
+        puts "Welcome to the Hiking Project CLI"
+        puts "---------------------------------"
+        get_coordinates
+    end
+
+    def get_coordinates
+        #get coordinates from user
+        puts "Please enter your latitude, longitude"
+
+        coord_input = ""
+        while coord_input == ""
+            coord_input = gets.strip
+        end
+
+        list_trails
+    end
+
+    def list_trails
+        #list trails based on user input
+        puts "Trails for latitude: x longitude: y"
+        puts "-----------------------------------------------"
+        puts "1 - Trail A name (xxx miles) - Trail A Summary"
+        puts "2 - Trail B name (xxx miles) - Trail A Summary"
+        puts "3 - Trail C name (xxx miles) - Trail A Summary"
+        puts "-----------------------------------------------"
+        menu
+    end
+
+    def menu
+        puts "Enter the number of the trail you would like more information on, list to relist, restart, or exit."
+
+        input = ""
+        while input == ""
+            input = gets.strip
+        end
+        
+        #allows user to relist trails, choose a trail to show detail, restart, or exit
+        if input == "restart"
+            call
+        elsif input == "list"
+            list_trails
+        elsif input == "exit"
+            goodbye
+        else
+            show_full_description
+        end
+    end
+
+    def show_full_description
+        #display trail full details
+        puts "-----------------------------------------------"
+        puts "Features: features1 features1 features1"
+        puts "Overview: overview1 overview1 overview1"
+        puts "Description: description1 description1 description1"
+        puts "-----------------------------------------------"
+        menu
+    end
+
+    def goodbye
+        puts "Thank you for using the Hiking Project CLI"
     end
 end
